f379c46a63076cf786bf1cc8fa133aa54e1c2d2c
Luis Felipe Zaguini <luis.ferreira@toodo.com.br>
2018-04-14 11:31:56 -0300

    fixed sizes, include 0

--- src/styles.js
+++ src/styles.js
@@ -1,6 +1,6 @@
 import { StyleSheet } from 'react-native'
 
-const sizes = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const sizes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 const positions = ['Left', 'Right', 'Top', 'Bottom', 'Horizontal', 'Vertical']
 
 function getVariations(attribute, gutterSize) {
@@ -9,7 +9,7 @@ function getVariations(attribute, gutterSize) {
   sizes.forEach((size) => {
     positions.forEach((position) => {
       for(let i = 0.25; i <= 1; i += 0.25) {
-        const variation = size * i
+        const variation = size === 0 ? i : size * i
 
         let attributeName = attribute.charAt(0)
         attributeName += position.charAt(0).toLowerCase()
