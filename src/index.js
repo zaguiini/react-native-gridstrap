bf57554d2a77691c7ab6b2a19fdd9c355563b7ce
Luis Felipe Zaguini <zaguini@Zaguinis-MacBook-Pro.local>
2019-03-10 11:04:56 -0300

    fxed

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
+import { Dimensions } from 'react-native'
 import generateStyles from './styles'
 import generateContainer from './container'
 import generateRow from './row'
 import generateCol from './col'
 
-export default (gutterSize = 16) => {
-  const {
-    container,
-    containerFluid,
-    row,
-    col,
-    ...styles,
-  } = generateStyles(gutterSize)
+const { width, height } = Dimensions.get('window')
+const realWidth = height > width ? width : height
+
+export default (gutterSize = 16, deviceBaseWidth = 375) => {
+  const { container, containerFluid, row, col, ...styles } = generateStyles(
+    Math.round((gutterSize * realWidth) / deviceBaseWidth)
+  )
 
   return {
     Container: generateContainer(container, containerFluid),
